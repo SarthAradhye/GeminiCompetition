853c9ba22e9fe6b29d102e653dd393d4b820c7c8
SarthAradhye <86707040+SarthAradhye@users.noreply.github.com>
2024-08-13 04:58:12 +0530

    Add files via upload
    Node files can not be uploaded

---
+++ script.js
@@ -0,0 +1,9 @@
+// Make sure to include these imports:
+// import { GoogleGenerativeAI } from "@google/generative-ai";
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+const prompt = "Tell me a joke";
+
+const result = await model.generateContent(prompt);
+console.log(result.response.text());
